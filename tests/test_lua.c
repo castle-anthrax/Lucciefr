1ff4c55dbd860caea7c006f915e38756cf21fb27
NiteHawk <n1tehawk@users.noreply.github.com>
2016-07-17 11:39:30 +0200

    Integrate LuaFileSystem (with symlinkattributes "target" extension)
    https://github.com/keplerproject/luafilesystem/commits/32b355a9902e6ce7aae68926fc3f2d05856fc299

--- tests/test_lua.c
+++ tests/test_lua.c
@@ -1,11 +1,19 @@
 #include "lua.h"
 #include "lualib.h"
 #include "lauxlib.h"
-#include "macro.h"
+
+#include "lfs.h"
+#include "luautils.h"
 
 void test_lua(void) {
 	lua_State *L = luaL_newstate();
 	luaL_openlibs(L);
-	UNUSED(luaL_dostring(L, "print('Hello ' .. jit.version)"));
+
+	luautils_dostring(L, "print('Hello ' .. jit.version)");
+#if _LINUX
+	luaopen_lfs(L);
+	luautils_dostring(L, "print(lfs.symlinkattributes('/proc/self/exe', 'target'))");
+#endif
+
 	lua_close(L);
 }
