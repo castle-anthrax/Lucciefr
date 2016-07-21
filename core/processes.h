226caf84e740d6bc97810d828d7627539951f034
NiteHawk <n1tehawk@users.noreply.github.com>
2016-07-21 16:19:48 +0200

    Fix the include guard from PROCESS_H to PROCESSES_H
    Also: Avoid potential conflict/redefinition of "getpid"
    (on Windows, and when including process.h)

--- core/processes.h
+++ core/processes.h
@@ -1,7 +1,7 @@
 /// @file processes.h
 
-#ifndef PROCESS_H
-#define PROCESS_H
+#ifndef PROCESSES_H
+#define PROCESSES_H
 
 #include <sys/types.h> // provides pid_t
 
@@ -10,8 +10,8 @@
 #include "luautils.h"
 
 #if _WINDOWS
+	#include <process.h> // getpid()
 	#include <windows.h>
-	#define getpid		GetCurrentProcessId
 #else
 	#include <unistd.h>
 #endif
@@ -41,4 +41,4 @@ char *get_pid_exe(pid_t pid, char *buffer, size_t size);
 
 LUA_CFUNC(luaopen_process); // Lua bindings
 
-#endif // PROCESS_H
+#endif // PROCESSES_H
