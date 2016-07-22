3aa3414c277067e53a525c2443c40dc556d02f89
NiteHawk <n1tehawk@users.noreply.github.com>
2016-07-22 08:59:59 +0200

    Add a console loop (thread) for interactive testing

--- tests/test_loop.c
+++ tests/test_loop.c
@@ -1,6 +1,32 @@
+#include "threads.h"
+
 #include <signal.h>
 
 static bool running = false;
+
+// interactive (and blocking!) console handler
+static THREAD_FUNC console_thread(void *arg) {
+	info("%s: STARTUP", __func__);
+	char input[128];
+	while (true) {
+		if (!fgets(input, sizeof(input), stdin))
+			break; // error or EOF
+		char *newline = strrchr(input, '\n');
+		if (newline) *newline = '\0'; // strip newline char
+
+		if (strcmp(input, "quit") == 0) {
+			running = false; // terminate main loop
+			break;
+		}
+		info("%s: %s", __func__, input);
+	}
+	/*
+	 * Note: It seems that Tea CI immediately encounters EOF on stdin, so
+	 * the above loop will exit - and this threads shuts down - right away!
+	 */
+	info("%s: SHUTDOWN", __func__);
+	thread_exit(0);
+}
 
 // an "interrupt" signal handler to gracefully end the timed loop
 static void ctrlc_handler(int signum) {
@@ -12,18 +38,18 @@ static void ctrlc_handler(int signum) {
 #define TICKS_MS	500
 
 void test_loop(int timeout) {
-	if (timeout < 0)
-		timeout = 3000; // workaround until we properly support "interactive"
-
 	signal(SIGINT, ctrlc_handler);
 	running = true;
+	pthread_t console = thread_start(console_thread, NULL, NULL);
 	while (running) {
 		int elapsed = get_elapsed_ms();
 		//extra("%s: elapsed = %d", __func__, elapsed);
 		extra("elapsed = %d", elapsed);
-		if (elapsed >= timeout)
+		if (timeout > 0 && elapsed >= timeout)
 			running = false;
 		else
 			Sleep(TICKS_MS);
 	}
+	thread_stop(console, 0);
+	thread_wait(console, 1000);
 }
